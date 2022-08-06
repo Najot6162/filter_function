cfbd334c300810dd934e8d99b8ff0628e5049de6
Biloliddin <najot6162@gmail.com>
2022-08-06 11:54:34 +0500

    added query for Product,Post

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -5,6 +5,32 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Product;
 
+
+class FilterController extends Filter
+{
+    public function filter()
+    {
+        $post = new Posts();
+        $queryPost = 'post2';
+
+        $product = new Products();
+        $queryProduct = [
+            'name' => 'iphone11pro',
+            'price' => '600$'
+        ];
+        $this->getDataWithFilter($product, $queryProduct);
+    }
+}
+
+class Filter
+{
+    public function getDataWithFilter($func, $query)
+    {
+        $func->filter($query);
+    }
+}
+
+
 class Products
 {
     public function filter($query)
@@ -20,30 +46,6 @@ class Posts
     {
         $post = Post::where('name', $query)->get();
         dd($post);
-    }
-}
-
-class Filter
-{
-    public function getDataWithFilter($func, $query)
-    {
-        $func->filter($query);
-    }
-}
-
-class FilterController extends Filter
-{
-    public function filter()
-    {
-        $post = new Posts();
-        $queryPost = 'post2';
-
-        $product = new Products();
-        $queryProduct = [
-            'name' => 'iphone11pro',
-            'price' => '600$'
-        ];
-        $this->getDataWithFilter($product, $queryProduct);
     }
 }
 
